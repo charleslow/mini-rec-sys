ddaaf7c7debef33371be7382f88f9dd357e200ce
Charles Low <charleslow88@gmail.com>
2023-06-30 12:55:08 +0000

    modified examples

--- examples/msmarco_bm25.py
+++ examples/msmarco_bm25.py
@@ -1,38 +1,49 @@
-from mini_rec_sys.scorers import BM25Scorer
+from mini_rec_sys.scorers import BM25Scorer, DenseScorer
 from mini_rec_sys.evaluators import Evaluator
-from mini_rec_sys.data import SessionCollection, Loader, Session
-import json
+from mini_rec_sys.sample_data import get_msmarco_sample_data
+from mini_rec_sys.data import Session, SessionDataset, ItemDataset
+from mini_rec_sys.encoders import MiniLmEncoder
 
-SESSIONS_FILE = "../sample_data/msmarco_reranking/msmarco_sample_sessions.json"
-ITEMS_FILE = "../sample_data/msmarco_reranking/msmarco_sample_items.json"
+items, sessions = get_msmarco_sample_data()
 
-# Loader to load text for each docid
-item_loader = Loader(id_name="item_id", data=ITEMS_FILE, load_fn=lambda x: {"text": x})
+# Put document texts into a ItemDataset
+item_dataset = ItemDataset(id_name="item_id", data=items, load_fn=lambda x: {"text": x})
 
-# Put relevance judgments into a SessionCollection
-with open(SESSIONS_FILE) as f:
-    sessions = json.load(f)
-collection = SessionCollection(
-    sessions=[
-        Session(
-            session_id=qid,
-            positive_items=v["positive_items"],
-            positive_relevances=v["relevances"],
-            negative_items=v["negative_items"],
-        )
-        for qid, v in sessions.items()
-    ],
-    item_loader=item_loader,
+# Put relevance judgments into a SessionDataset
+collection = SessionDataset(
+    id_name="session_id",
+    data=sessions,
+    item_dataset=item_dataset,
+    store_fn=lambda id, row: Session(
+        query=row["query"],
+        session_id=id,
+        positive_items=row["positive_items"],
+        positive_relevances=row["relevances"],
+        negative_items=row["negative_items"],
+    ),
 )
 
 # Simple BM25 scorer for reranking
 bm25_scorer = BM25Scorer(
     query_key="query",
     test_documents_key="item_attributes",
-    train_documents={qid: {"text": v} for qid, v in iter(item_loader)},
+    train_documents={qid: v for qid, v in iter(item_dataset)},
     fields=["text"],
 )
 
 # Evaluate performance
 ndcg, se = Evaluator(bm25_scorer, collection).evaluate()
 print(f"BM25 reranking ndcg is: {ndcg:.4f}+-{se:.4f}")
+
+# Simple minilm scorer for reranking
+minilm_encoder = MiniLmEncoder(dim_embed=384, max_length=64)
+minilm_scorer = DenseScorer(
+    query_key="query",
+    test_documents_key="item_attributes",
+    passage_text_key="text",
+    q_encoder=minilm_encoder,
+    p_encoder=minilm_encoder,
+    batch_size=32,
+)
+ndcg, se = Evaluator(minilm_scorer, collection).evaluate()
+print(f"Minilm reranking ndcg is {ndcg:.4f}+-{se:.4f}")
